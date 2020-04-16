5b218f3c0258253317b82149536fcfdec8cc805a
DeStilleGast <3677706+DeStilleGast@users.noreply.github.com>
2020-04-16 17:15:50 +0200

    Cleanup CommandHandler.java

--- src/main/java/hoeve/plugins/werewolf/CommandHandler.java
+++ src/main/java/hoeve/plugins/werewolf/CommandHandler.java
@@ -1,11 +1,8 @@
 package hoeve.plugins.werewolf;
 
-import hoeve.plugins.werewolf.WerewolfPlugin;
 import hoeve.plugins.werewolf.game.EnumDeadType;
 import hoeve.plugins.werewolf.game.WerewolfGame;
 import hoeve.plugins.werewolf.game.WerewolfPlayer;
-import hoeve.plugins.werewolf.game.actions.NearbySelector;
-import hoeve.plugins.werewolf.game.interfaces.CupidoScreen;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -13,6 +10,7 @@ import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -23,76 +21,60 @@ public class CommandHandler implements CommandExecutor, TabCompleter {
     private final WerewolfGame werewolfGame;
     private final WerewolfPlugin plugin;
 
-//    private WerewolfScreen wwScreen;
-    private CupidoScreen cupidoScreen;
-    private NearbySelector nearbySelector;
-
 
     public CommandHandler(WerewolfGame werewolfGame, WerewolfPlugin plugin) {
         this.werewolfGame = werewolfGame;
         this.plugin = plugin;
     }
 
-    private void notify(CommandSender sender, String message){
-        sender.sendMessage(message);
+    private void notify(CommandSender sender, String message) {
+        werewolfGame.notifyPlayer(sender, message);
     }
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if(sender instanceof Player) {
+        if (sender instanceof Player) {
             Player player = (Player) sender;
             boolean hasLeader = werewolfGame.getGameMaster() != null;
 
-            if(args.length == 0){
-
+            if (args.length == 0) {
                 return true;
             }
 
-            switch (args[0].toLowerCase()){
-                case "new":
-
-                    if(hasLeader){
+            if (hasLeader) {
+                switch (args[0].toLowerCase()) {
+                    case "new":
                         notify(sender, "There is already a game running");
-                    }else{
-                        werewolfGame.setGameMaster(player);
-                        plugin.getScoreboardManager().updateScoreboards(werewolfGame);
-                    }
 
-                    break;
-                case "join":
-                    if(hasLeader){
-                        if(!werewolfGame.addPlayer(player)){
+                        break;
+                    case "join":
+                        if (!werewolfGame.addPlayer(player)) {
                             notify(player, "You are already in the game, please wait...");
-                        }else{
+                        } else {
                             plugin.getScoreboardManager().updateScoreboards(werewolfGame);
+                            notify(player, "You have joined the game");
                         }
-                    }else{
-                        notify(player, "There is no game active");
-                    }
-                    break;
-                case "leave":
-                    if(werewolfGame.removePlayer(player)){
-                        notify(player, "You left the game");
-                        plugin.getScoreboardManager().updateScoreboards(werewolfGame);
 
-                    }else{
-                        notify(player, "You are not even in the game");
-                    }
-                    break;
-                case "vote":
-                    if(hasLeader) {
+                        break;
+                    case "leave":
+                        if (werewolfGame.removePlayer(player)) {
+                            notify(player, "You left the game");
+                            plugin.getScoreboardManager().updateScoreboards(werewolfGame);
+                        } else {
+                            notify(player, "You are not even in the game");
+                        }
+                        break;
+                    case "vote":
                         if (sender.equals(werewolfGame.getGameMaster().getPlayer())) {
                             werewolfGame.startDayVote();
                         } else {
                             werewolfGame.showDayVote(player);
                         }
-                    }
 
-                    break;
-                case "start":
-                    if(hasLeader) {
-                        werewolfGame.centerPlayers();
+                        break;
+                    case "start":
                         werewolfGame.startGame();
+                        werewolfGame.centerPlayers();
 //
 //                        if (nearbySelector == null) {
 //                            nearbySelector = new NearbySelector(werewolfGame, WerewolfRole.class);
@@ -101,7 +83,6 @@ public class CommandHandler implements CommandExecutor, TabCompleter {
 //                            nearbySelector.stop();
 //                            werewolfGame.notifyGameMaster(nearbySelector.getTopSelectedPlayer().getName() + " was selected");
 //                        }
-
 
 
 //                    if(oracleScreen == null) {
@@ -117,43 +98,43 @@ public class CommandHandler implements CommandExecutor, TabCompleter {
 //                    wwScreen.prepareInventory(werewolfGame);
 
 //                    werewolfGame.getPlayerList().forEach(w -> wwScreen.openInventory((Player) w.getPlayer()));
-                    }else{
-                        notify(player, "There is no game active");
-                    }
-                    break;
-                case "center":
-                    werewolfGame.centerPlayers();
-                    break;
-                case "night":
-                    werewolfGame.startDefaultNightActivities();
-                    break;
-                case "roles":
-                    for (WerewolfPlayer pl : werewolfGame.getPlayerList()) {
-                        notify(sender, pl.getPlayer().getName() + " -  " + pl.getRole().getRoleName());
-                    }
-                    break;
-                case "kill":
-                    if(hasLeader) {
+                        break;
+                    case "center":
+                        werewolfGame.centerPlayers();
+                        break;
+                    case "night":
+                        werewolfGame.startDefaultNightActivities();
+                        break;
+                    case "roles":
+                        for (WerewolfPlayer pl : werewolfGame.getPlayerList()) {
+                            notify(sender, pl.getPlayer().getName() + " -  " + pl.getRole().getRoleName());
+                        }
+                        break;
+                    case "kill":
                         if (sender.equals(werewolfGame.getGameMaster().getPlayer())) {
-                            if(args.length == 1){
+                            if (args.length == 1) {
                                 notify(sender, "Please specify a name");
-                            }else {
+                            } else {
                                 WerewolfPlayer target = werewolfGame.getPlayerByName(args[1]);
-                                if(target != null){
+                                if (target != null) {
                                     werewolfGame.getDeathTeller().addDeath(target.getPlayer(), EnumDeadType.GAMEMASTER);
-                                }else{
+                                } else {
                                     notify(sender, "Could not find player");
                                 }
                             }
                         }
-                    }
-            }
 
+                }
+            } else {
+                if ("new".equals(args[0].toLowerCase()) && player.hasPermission("werewolf.gamemaster")) {
+                    werewolfGame.setGameMaster(player);
+                    plugin.getScoreboardManager().updateScoreboards(werewolfGame);
+                }
+            }
 
 
 //            wwScreen.prepareInventory(werewolfGame);
 //            wwScreen.openInventory(player);
-
 
 
 //            plugin.setupWaiter(1, 30, () -> {
@@ -173,6 +154,14 @@ public class CommandHandler implements CommandExecutor, TabCompleter {
 //                return Lists.asList("new", "start", "join");
 //        }
 
-        return Arrays.asList("new", "start", "join", "leave");
+        if(werewolfGame.getGameMaster() != null){
+            return Collections.singletonList("new");
+        }
+
+        if(sender.hasPermission("werewolf.gamemaster")){
+            return Arrays.asList("start", "night", "vote");
+        }
+
+        return Arrays.asList("vote", "leave");
     }
 }
